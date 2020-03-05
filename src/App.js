383fe38c486e995d8fa08bc3522844d8de3becee
z1gonzo <goonsky@gmail.com>
2020-03-06 00:29:35 +0100

    Add to state, display and clear input implemented

--- src/App.js
+++ src/App.js
@@ -16,7 +16,11 @@ class App extends Component {
     super();
     this.state = {
       message: "Hello from state",
-      newTodo: ""
+      newTodo: "",
+      todos: [{
+        title: "Learn React",
+        done: false
+      }]
     }
   }
 
@@ -30,19 +34,35 @@ class App extends Component {
   formSubmitted(event) {
     event.preventDefault();
     console.log(this.state.newTodo)
+    this.setState({
+      newTodo: "",
+      todos: [...this.state.todos, {
+        title: this.state.newTodo,
+        done: false
+      }]
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <Welcome name="Functional component"/>
-        <Welcome2 name="Class Component"/>
+        {/* <Welcome name="Functional component"/> */}
+        {/* <Welcome2 name="Class Component"/> */}
         <h1>{this.state.message}</h1>
         <form onSubmit={(event) => this.formSubmitted(event)}>
           <label htmlFor="newTodo">New Todo</label>
-          <input onChange={(event) => this.newTodoChanged(event)} id="newTodo" name="newTodo"/>
+          <input 
+            onChange={(event) => this.newTodoChanged(event)} 
+            id="newTodo" 
+            name="newTodo" 
+            value={this.state.newTodo}/>
           <button type="submit">Add new Todo</button>
         </form>
+        <ul>
+          {this.state.todos.map(todo => {
+            return <li key={todo.title}>{todo.title}</li>
+          })}
+        </ul>
       </div>
     );
   }
