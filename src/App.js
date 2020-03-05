c3779c4ae493299bb5735b43a93a4d0d006b7c84
z1gonzo <goonsky@gmail.com>
2020-03-05 23:58:02 +0100

    Func/Class Comps & this transfer with =>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Siemka Componentos mucho</h1>
-    </div>
-  );
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+class Welcome2 extends React.Component {
+  render() {
+    return <h1>Hello2, {this.props.name}</h1>;
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: "Hello from state",
+      newTodo: ""
+    }
+  }
+
+  newTodoChanged(event) {
+    // console.log(event.target.value)
+    this.setState({
+      newTodo: event.target.value
+    })
+  }
+
+  formSubmitted(event) {
+    event.preventDefault();
+    console.log(this.state.newTodo)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Welcome name="Functional component"/>
+        <Welcome2 name="Class Component"/>
+        <h1>{this.state.message}</h1>
+        <form onSubmit={(event) => this.formSubmitted(event)}>
+          <label htmlFor="newTodo">New Todo</label>
+          <input onChange={(event) => this.newTodoChanged(event)} id="newTodo" name="newTodo"/>
+          <button type="submit">Add new Todo</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
